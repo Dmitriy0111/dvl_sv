642a778d0dc93f5b6a23a350a87c0f866ea95de4
dmitriy0111 <vlasovdv0111@mail.ru>
2019-12-28 14:47:48 +0700

    Revert "Revert "Revert "Changed oop testbench"""
    This reverts commit 2231ecb86840e0ad94851c16d34717f1ef977b73.

--- dvv_classes/dvv_mon.sv
+++ dvv_classes/dvv_mon.sv
@@ -14,20 +14,12 @@ class dvv_mon #(type seq_type) extends dvv_bc;
 
     dvv_sock    #(seq_type)     item_sock;
 
-    extern         function new(string name = "", dvv_bc parent = null);
-
-    extern virtual task     build();
-    extern virtual task     run();
+    extern function new(string name = "", dvv_bc parent = null);
     
 endclass : dvv_mon
 
 function dvv_mon::new(string name = "", dvv_bc parent = null);
     super.new(name,parent);
 endfunction : new
-
-task dvv_mon::build();
-endtask : build
-task dvv_mon::run();
-endtask : run
 
 `endif // DVV_MON__SV
