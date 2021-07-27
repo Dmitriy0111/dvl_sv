0111665663524688a2e5ad5cc8b8e9125fb05b28
dmitriy0111 <vlasovdv0111@mail.ru>
2021-07-27 21:40:27 +0700

    Changed camera link base driver class

--- models/camera_link_base/clb_drv.svh
+++ models/camera_link_base/clb_drv.svh
@@ -13,13 +13,11 @@
 /*
     | Line/Bit |   0   |   1   |   2   |   3   |   4   |   5   |   6   |
     |----------|-------|-------|-------|-------|-------|-------|-------|
-    | 0        | G0    | R5    | R4    | R3    | R2    | R1    | R0    |
-    | 1        | B1    | B0    | G5    | G4    | G3    | G2    | G1    |
-    | 2        | DVAL  | FVAL  | LVAL  | B5    | B4    | B3    | B2    |
     | 3        | Spare | B7    | B6    | G7    | G6    | R7    | R6    |
+    | 2        | DVAL  | FVAL  | LVAL  | B5    | B4    | B3    | B2    |
+    | 1        | B1    | B0    | G5    | G4    | G3    | G2    | G1    |
+    | 0        | G0    | R5    | R4    | R3    | R2    | R1    | R0    |
 */
-
-`timescale 1ps/1ps
 
 class clb_drv extends dvl_bc;
     `OBJ_BEGIN( clb_drv )
@@ -42,23 +40,28 @@ class clb_drv extends dvl_bc;
     bit     [23 : 0]    RGBV;
     int                 CTRL;
 
-    int         LVAL_F = 32'b001;
-    int         FVAL_F = 32'b010;
-    int         DVAL_F = 32'b100;
+    int                 LVAL_F = 32'b001;
+    int                 FVAL_F = 32'b010;
+    int                 DVAL_F = 32'b100;
 
-    int         fve2fvs = 10;   // frame valid end to frame valid start
-    int         fvs2flv = 10;   // frame valid start to first line valid
-    int         llv2fve = 10;   // last line valid to frame valid end
-    int         lvs2dvs = 10;   // line valid start to data valid start
-    int         lv2lv   = 10;   // line valid to line valid
-    int         dve2lve = 10;   // data valid end to line valid end
+    int         wtbsftr = 15000;    // wait time before start first transaction
+    int         fve2fvs = 10;       // frame valid end to frame valid start
+    int         fvs2flv = 10;       // frame valid start to first line valid
+    int         llv2fve = 10;       // last line valid to frame valid end
+    int         lvs2dvs = 10;       // line valid start to data valid start
+    int         lv2lv   = 10;       // line valid to line valid
+    int         dve2lve = 10;       // data valid end to line valid end
 
     int         tx_comp [5];
+
+    process     main_proc;
 
     extern function new(string name = "", dvl_bc parent = null);
 
     extern task build();
     extern task run();
+
+    extern task run_main();
 
     extern task set_tx_components();
     extern task drv_lines();
@@ -70,8 +73,6 @@ function clb_drv::new(string name = "", dvl_bc parent = null);
     super.new(name,parent);
     // clock component
     tx_comp[0] = 7'b1100011;
-
-    $timeformat(-12,3,"ps",20);
 endfunction : new
 
 task clb_drv::build();
@@ -97,6 +98,8 @@ task clb_drv::build();
     if( !dvl_res_db#(virtual clb_lvds_if)::get_res_db(this, "", "lvds_if", vif) )
         $fatal();
     // Timing settings
+    if( !dvl_res_db#(int)::get_res_db(this, "", "wtbsftr", wtbsftr) )
+        wtbsftr = 15000;
     if( !dvl_res_db#(int)::get_res_db(this, "", "fve2fvs", fve2fvs) )
         fve2fvs = 10;
     if( !dvl_res_db#(int)::get_res_db(this, "", "fvs2flv", fvs2flv) )
@@ -121,7 +124,12 @@ task clb_drv::build();
 endtask : build
 
 task clb_drv::run();
+    forever
+        run_main();
+endtask : run
 
+task clb_drv::run_main();
+    
     vif.clk_p = '1;
     vif.clk_n = '0;
 
@@ -138,10 +146,28 @@ task clb_drv::run();
     vif.d3_n = '0;
 
     @(posedge vif.rst);
-    forever begin
-        drv_main();
-    end
-endtask : run
+    fork
+        begin : drive_lines
+            main_proc = process::self();
+            CTRL = '0;
+            RGBV = '0;
+            set_tx_components();
+            repeat(wtbsftr) begin
+                drv_lines();
+            end
+        
+            while(1) begin
+                drv_main();
+            end
+        end
+        begin : wait_reset
+            @(negedge vif.rst);
+            matrix_in.reset_pos();
+            main_proc.kill();
+        end
+    join_any
+    
+endtask : run_main
 
 task clb_drv::set_tx_components();
     // RGBV[23 : 16] - R
@@ -183,40 +209,45 @@ task clb_drv::drv_lines();
         tx_comp[4] <<= 1;
         @(negedge vif.clk);
     end
+    tx_comp[0] >>= 7;
+    tx_comp[1] >>= 7;
+    tx_comp[2] >>= 7;
+    tx_comp[3] >>= 7;
+    tx_comp[4] >>= 7;
 endtask : drv_lines
 
 task clb_drv::drv_main();
+    CTRL = '0;
+    set_tx_components();
     repeat(fve2fvs) begin
-        CTRL = '0;
-        set_tx_components();
         drv_lines();
     end
+    CTRL = FVAL_F;
+    set_tx_components();
     repeat(fvs2flv) begin
-        CTRL = FVAL_F;
-        set_tx_components();
         drv_lines();
     end
     for( Height_i = 0 ; Height_i < Height ; Height_i++ ) begin
+        CTRL = FVAL_F | LVAL_F;
+        set_tx_components();
         repeat(lvs2dvs) begin
-            CTRL = FVAL_F | LVAL_F;
-            set_tx_components();
             drv_lines();
         end
+        CTRL = FVAL_F | LVAL_F | DVAL_F;
         repeat(Width) begin
-            CTRL = FVAL_F | LVAL_F | DVAL_F;
             matrix_in.get_image_RGB( RGBV );
             set_tx_components();
             drv_lines();
         end
         RGBV = '0;
+        CTRL = FVAL_F | LVAL_F;
+        set_tx_components();
         repeat(dve2lve) begin
-            CTRL = FVAL_F | LVAL_F;
-            set_tx_components();
             drv_lines();
         end
+        CTRL = FVAL_F;
+        set_tx_components();
         repeat( Height_i == ( Height - 1 ) ? llv2fve : lv2lv ) begin
-            CTRL = FVAL_F;
-            set_tx_components();
             drv_lines();
         end
     end
